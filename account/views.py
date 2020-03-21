ee9e8e46a88977b7b89a0be931ae17751e624849
jongwooo <han980817@gmail.com>
2020-03-21 15:07:25 +0900

    Update account/views.py

--- account/views.py
+++ account/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 # Create your views here.
 def login(request):
-  return render(request, "account/login.html")
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = auth.authenticate(request, username=username, password=password)
+    if user is not None:
+      auth.login(request, user)
+      return redirect('home')
+    else:
+      return render(request, 'account/login.html', {'error': '아이디와 비밀번호를 확인해주세요.'})
+  else:
+    return render(request, 'account/login.html')
 
 def signup(request):
+  if request.method == 'POST':
+    if request.POST['password'] == request.POST['confirm']:
+      user = User.objects.create_user(
+        request.POST['username'], password=request.POST['password'])
+      auth.login(request, user)
+      return redirect('home')
+    else:
+      return render(request, "account/signup.html", {'error': '비밀번호를 다시 확인해주세요.'})
   return render(request, "account/signup.html")
+
+def logout(request):
+  auth.logout(request)
+  return redirect('home')
