7b48df0e8759fe5223ab463972e7734b89baaf04
CHOI0212 <choiys0311@gmail.com>
2020-03-28 22:36:28 +0900

    media file setting

--- config/urls.py
+++ config/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import page.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', page.views.home, name='home'),
     path('account/', include('account.urls')),
     path('vote/', include('vote.urls')),
-]
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
