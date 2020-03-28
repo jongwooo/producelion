d263b3d89db4eaa5299f7fe2f0e059059fb4367a
CHOI0212 <choiys0311@gmail.com>
2020-03-28 22:46:22 +0900

    add media image file

--- account/models.py
+++ account/models.py
@@ -4,9 +4,10 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete = models.CASCADE)
+    image = models.ImageField(upload_to='images/',default =True)
     name = models.CharField(max_length=10,blank=True)
-    management = models.TextField(max_length=20,blank=True)
-    location = models.CharField(max_length=10,blank=True)
+    management = models.TextField(max_length=30,blank=True)
+    location = models.CharField(max_length=5,blank=True)
 
     def __str__(self):
         return self.name
