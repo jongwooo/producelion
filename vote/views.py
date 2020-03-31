46eac0a078bc5bc4e21f3548be52ffefc7cb42c8
CHOI0212 <choiys0311@gmail.com>
2020-03-31 17:21:09 +0900

    vote page change

--- vote/views.py
+++ vote/views.py
@@ -4,27 +4,27 @@ from .models import VoteList
 from django.contrib.auth.models import User
 
 def vote(request):
-  picks = Profile.objects
-  return render(request, "vote/list.html",{'picks':picks})
+  picks = Profile.objects.all()
+  if not picks:
+    return render(request, "vote/nolist.html")
+  else:
+    return render(request, "vote/list.html",{'picks':picks})
 
 def complete(request):
     nowuser = VoteList.objects.filter(user=request.user)
-    try:
-      if not nowuser:
-        myvote = VoteList()
-        myvote.user = request.user
-        myvote.myname = request.user.username
-        myvote.team = request.POST.get('pick')
-        myvote.votenum += 1
-        myvote.save()
+    if not nowuser:
+      myvote = VoteList()
+      myvote.user = request.user
+      myvote.myname = request.user.username
+      myvote.team = request.POST.get('pick')
+      myvote.votenum += 1
+      myvote.save()
+    else:
+      obj = VoteList.objects.get(user = request.user)
+      if obj.lastteam == request.POST.get('pick'):
+        return render(request,"vote/error.html")
       else:
-        obj = VoteList.objects.get(user = request.user)
-        if obj.lastteam == request.POST.get('pick'):
-          return render(request,"vote/error.html")
-        else:
-          obj.team = request.POST.get('pick')
-          obj.votenum += 1
-          obj.save()
-      return render(request,"vote/complete.html")
-    except:
-      return render(request,"vote/nolist.html")
+        obj.team = request.POST.get('pick')
+        obj.votenum += 1
+        obj.save()
+    return render(request,"vote/complete.html")
